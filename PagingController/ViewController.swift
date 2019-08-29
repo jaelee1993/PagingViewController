e7385e2a00101915ff646ae6c3fde214d0fdbf21
JLee <jlee@accent-technologies.com>
2019-08-29 17:29:10 -0400

    first commit

--- PagingController/ViewController.swift
+++ PagingController/ViewController.swift
@@ -8,13 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import UIKit
 
+class ViewController1: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor.yellow
+        
+        // label
+        let labelInst = UILabel()
+        self.view.addSubview(labelInst)
+        labelInst.text = "Page 1"
+        labelInst.translatesAutoresizingMaskIntoConstraints = false
+        labelInst.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50).isActive = true
+        labelInst.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 20).isActive = true
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
 
+class ViewController2: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.yellow
+        
+        // label
+        let labelInst = UILabel()
+        self.view.addSubview(labelInst)
+        labelInst.text = "Page 1"
+        labelInst.translatesAutoresizingMaskIntoConstraints = false
+        labelInst.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50).isActive = true
+        labelInst.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 20).isActive = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+class ViewController3: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.yellow
+        
+        // label
+        let labelInst = UILabel()
+        self.view.addSubview(labelInst)
+        labelInst.text = "Page 1"
+        labelInst.translatesAutoresizingMaskIntoConstraints = false
+        labelInst.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50).isActive = true
+        labelInst.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 20).isActive = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
